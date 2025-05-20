f1156effd3031023b99234960882a31a6805a3f0
Sami Gündoğan <gundogannsami@gmail.com>
2025-05-20 15:17:04 +0300

    add:TabBar

---
+++ News/Scenes/Home/TabBar.swift
@@ -0,0 +1,43 @@
+//
+//  TabBar.swift
+//  News
+//
+//  Created by Sami Gündoğan on 20.05.2025.
+//
+
+import UIKit
+
+final class TabBar: UITabBarController {
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabs()
+    }
+}
+
+// MARK: - Private Methods
+
+private extension TabBar {
+    func setupTabs() {
+        let homeVC = createNav(with: "News", and: UIImage(systemName: "newspaper.fill"), viewController: HomeVC())
+        
+        let settingsVC = createNav(with: "Settings", and: UIImage(systemName: "gearshape"), viewController: SettingsVC())
+        
+        setViewControllers([homeVC, settingsVC], animated: false)
+        tabBar.tintColor = .systemBlue
+        tabBar.backgroundColor = .systemBackground
+    }
+
+    func createNav(with title: String, and image: UIImage?, viewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.tabBarItem.title = title
+        nav.tabBarItem.image = image
+        viewController.title = title
+        nav.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.largeTitleDisplayMode = .always
+        
+        return nav
+    }
+}
