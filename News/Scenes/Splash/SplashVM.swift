43b7f2f2298b0eb940730334b479f2db6e1e0e21
Sami Gündoğan <gundogannsami@gmail.com>
2025-05-20 16:32:54 +0300

    add: SplashVM protocol input

--- News/Scenes/Splash/SplashVM.swift
+++ News/Scenes/Splash/SplashVM.swift
@@ -7,13 +7,16 @@
 
 import Foundation
 
-protocol SplashVMProtocol: AnyObject{
+protocol SplashVMProtocol: AnyObject {
     func splashScreen()
 }
 
 final class SplashVM: SplashVMProtocol {
     
     weak var output: SplashVMProtocol?
+    weak var input: SplashVMProtocol?
+    
+    init() { input = self }
     
     func splashScreen() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
