06388f19f9d67b7820b4f7af6ec67ebc1c52cf93
Sami Gündoğan <gundogannsami@gmail.com>
2025-06-19 09:26:11 +0300

    add:settingsVC table view

---
+++ News/Models/SettingsModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsModel.swift
+//  News
+//
+//  Created by Sami Gündoğan on 18.06.2025.
+//
+
+import Foundation
--- News/Scenes/Settings/SettingsVC.swift
+++ News/Scenes/Settings/SettingsVC.swift
@@ -17,4 +17,32 @@ protocol SettingsVMOutputProtocol: AnyObject {
 
 final class SettingsVC: UIViewController {
     
+    // MARK: Properties
+
+    private let viewModel: SettingsVM
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
+    // MARK: Inits
+
+    init(viewModel: SettingsVM = SettingsVM()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel.outputDelegate = self
+    }
+
+    required init?(coder: NSCoder) { fatalError() }
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
 }
