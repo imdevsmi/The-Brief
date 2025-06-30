d6bbd62abbe491ca2745274d8f05aadc7d5f6eb3
Sami Gündoğan <gundogannsami@gmail.com>
2025-06-30 15:41:41 +0300

    edit: configure image add animation

--- News/Extensions/configureImage .swift
+++ News/Extensions/configureImage .swift
@@ -10,17 +10,39 @@ import UIKit
 
 extension UIImageView {
     func setImage(with urlString: String?, placeholder: UIImage? = nil, targetSize: CGSize? = nil) {
-        guard let urlString = urlString, let url = URL(string: urlString) else {
+        guard let urlString, let url = URL(string: urlString) else {
             self.image = placeholder
-            
             return
         }
-        
-        self.kf.setImage(with: url,placeholder: placeholder, options: [
-                .scaleFactor(UIScreen.main.scale),
-                .cacheOriginalImage,
-                .transition(.fade(0.1)),
-                .memoryCacheExpiration(.seconds(300)),
-                .backgroundDecode])
+
+        var options: KingfisherOptionsInfo = [
+            .scaleFactor(UIScreen.main.scale),
+            .cacheOriginalImage,
+            .transition(.fade(0.4)),
+            .memoryCacheExpiration(.seconds(300)),
+            .backgroundDecode
+        ]
+
+        if let targetSize {
+            let processor = ResizingImageProcessor(referenceSize: targetSize, mode: .aspectFill)
+            options.append(.processor(processor))
+        }
+
+        kf.setImage(with: url, placeholder: placeholder, options: options) { result in
+            switch result {
+            case .success(let value):
+                if value.cacheType == .none {
+                    self.alpha = 0
+                    self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+
+                    UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0.8, options: [.curveEaseOut],
+                                   animations: {
+                        self.alpha = 1
+                        self.transform = .identity})
+                }
+            case .failure:
+                break
+            }
+        }
     }
 }
