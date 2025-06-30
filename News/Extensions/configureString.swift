ff0abdc83bcf23c3c9245b522852e133e2c6fc2c
Sami Gündoğan <gundogannsami@gmail.com>
2025-06-30 15:31:24 +0300

    edit: configure string

--- News/Extensions/configureString.swift
+++ News/Extensions/configureString.swift
@@ -8,33 +8,42 @@
 import Foundation
 
 extension String {
+    
     func timeAgoSinceDate() -> String? {
         let isoFormatter = ISO8601DateFormatter()
         guard let date = isoFormatter.date(from: self) else { return nil }
 
         let now = Date()
-        let components = Calendar.current.dateComponents([.minute, .hour, .day, .weekOfYear], from: date, to: now)
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.minute, .hour, .day, .weekOfYear], from: date, to: now)
 
-        if let week = components.weekOfYear, week > 0 {
+        let week = components.weekOfYear ?? 0
+        let day = components.day ?? 0
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+
+        switch true {
+        case week > 0:
             return "\(week)w ago"
-        } else if let day = components.day, day > 0 {
+        case day > 0:
             return "\(day)d ago"
-        } else if let hour = components.hour, hour > 0 {
-            let minute = components.minute ?? 0
-            return minute > 0 ? "\(hour)h \(minute)m ago" : "\(hour)h ago"
-        } else if let minute = components.minute, minute > 0 {
+        case hour > 0 && minute > 0:
+            return "\(hour)h \(minute)m ago"
+        case hour > 0:
+            return "\(hour)h ago"
+        case minute > 0:
             return "\(minute)m ago"
-        } else {
+        default:
             return "Just now"
         }
     }
-
+    
     func formattedHourAndMinute() -> String? {
         let isoFormatter = ISO8601DateFormatter()
         guard let date = isoFormatter.date(from: self) else { return nil }
-
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm"
-        return dateFormatter.string(from: date)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: date)
     }
 }
