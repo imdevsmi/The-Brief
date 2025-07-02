570220f8e9ad56985462fd2c9266eb2086fc6089
Sami Gündoğan <gundogannsami@gmail.com>
2025-07-02 19:22:54 +0300

    edit: tab bar animation

--- News/Scenes/Home/TabBar.swift
+++ News/Scenes/Home/TabBar.swift
@@ -25,10 +25,33 @@ private extension TabBar {
         let settingsVC = createNav(with: "Settings", and: UIImage(systemName: "gear"), viewController: SettingsVC())
         
         setViewControllers([homeVC, settingsVC], animated: false)
+        
         tabBar.tintColor = .systemBlue
-        tabBar.backgroundColor = .systemBackground
+        tabBar.unselectedItemTintColor = .systemGray3
+        tabBar.backgroundColor = .clear
+        
+        let blurEffect = UIBlurEffect(style: .systemChromeMaterial)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = tabBar.bounds
+        blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tabBar.insertSubview(blurView, at: 0)
+        
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundEffect = blurEffect
+        appearance.shadowColor = UIColor.systemGray4
+        appearance.stackedLayoutAppearance.selected.iconColor = .systemBlue
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.systemBlue]
+        
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: -1)
+        tabBar.layer.shadowRadius = 6
+        tabBar.layer.shadowColor = UIColor.black.cgColor
+        tabBar.layer.shadowOpacity = 0.05
+        tabBar.layer.masksToBounds = false
     }
-
+    
     func createNav(with title: String, and image: UIImage?, viewController: UIViewController) -> UINavigationController {
         let nav = UINavigationController(rootViewController: viewController)
         nav.tabBarItem.title = title
