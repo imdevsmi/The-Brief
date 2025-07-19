cc638c2cde72a35a3a9e8e6a6101a7303a253a21
Sami Gündoğan <gundogannsami@gmail.com>
2025-07-19 15:21:17 +0300

    add: open browser ui activity

--- News/Utilities/openBrowser.swift
+++ News/Utilities/openBrowser.swift
@@ -13,4 +13,27 @@ final class openBrowser: UIActivity {
     override var activityTitle: String? {
         "Open in Safari"
     }
+    
+    override var activityImage: UIImage? {
+        UIImage(systemName: "safari")
+    }
+    
+    override var activityType: UIActivity.ActivityType? {
+        UIActivity.ActivityType("https://www.linkedin.com/in/samigundogan/")
+    }
+    
+    override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
+        activityItems.contains { $0 is URL }
+    }
+    
+    override func prepare(withActivityItems activityItems: [Any]) {
+        url = activityItems.first(where: { $0 is URL }) as? URL
+    }
+    
+    override func perform() {
+        if let url = url {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        activityDidFinish(true)
+    }
 }
