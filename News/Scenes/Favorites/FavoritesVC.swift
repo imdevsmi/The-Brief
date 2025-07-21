f0b331f0378d2cc0c2193337edbf595f7d22776b
Sami Gündoğan <gundogannsami@gmail.com>
2025-07-21 14:46:04 +0300

    add: FaoritesVC properties

--- News/Scenes/Favorites/FavoritesVC.swift
+++ News/Scenes/Favorites/FavoritesVC.swift
@@ -9,5 +9,24 @@ import Foundation
 import UIKit
 
 final class FavoritesVC: UIViewController {
+    private let viewModel: FavoritesVM
     
+    // MARK: Properties
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.register(NewsCell.self, forCellReuseIdentifier: NewsCell.reuseIdentifier)
+        tableView.rowHeight = 152
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
+    init(viewModel: FavoritesVM) {
+                
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
