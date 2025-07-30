dbae6a18e0e7d61fd01549afc0f7a9c6c682d41a
Sami Gündoğan <gundogannsami@gmail.com>
2025-07-30 17:06:32 +0300

    add: favorites manager protocol

--- News/Scenes/Favorites/FavoritesManager.swift
+++ News/Scenes/Favorites/FavoritesManager.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 
-enum FavoritesError {
+enum FavoritesError: Error {
     case error
 }
 
 enum FavoritesKey: String {
     case favoritesArticle = "favoritesArticle"
     case theme = "theme"
+}
+
+protocol FavoritesManagerProtocol {
+    func save<T: Codable>(item: T)
+    func fetchAll<T: Codable>(completion: @escaping(Result<T, FavoritesError>) -> Void)
+    func removeAll()
 }
