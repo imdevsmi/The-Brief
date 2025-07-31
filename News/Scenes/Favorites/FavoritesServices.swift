208010966d3299f884c01435fada263acd48ad5e
Sami Gündoğan <gundogannsami@gmail.com>
2025-07-31 11:38:29 +0300

    add: favorites services protocol

--- News/Scenes/Favorites/FavoritesServices.swift
+++ News/Scenes/Favorites/FavoritesServices.swift
@@ -8,7 +8,10 @@
 import Foundation
 
 protocol FavoritesServicesProtocol {
-    
+    func saveFavoriteArtice(_ article: Article, completion: @escaping (Result<Void, FavoritesError>) -> Void)
+    func fetchFavorites(completion: @escaping(Result<[Article], FavoritesError>)->Void)
+    func updateFavorites(articles: [Article])
+    func createFavoriteArticleDatabase()
 }
 
 final class FavoriteService {
