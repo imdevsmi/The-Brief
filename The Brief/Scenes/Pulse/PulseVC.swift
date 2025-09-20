0361bb1d00eb6a732994e94b83896c5f7396cb6e
Sami Gündoğan <hsnsmi51@gmail.com>
2025-09-20 21:06:59 +0300

    add: PulseVC and PulseVM swift file

---
+++ The Brief/Scenes/Pulse/PulseVC.swift
@@ -0,0 +1,8 @@
+//
+//  PulseVC.swift
+//  The Brief
+//
+//  Created by Sami Gündoğan on 20.09.2025.
+//
+
+import Foundation
---
+++ The Brief/Scenes/Pulse/PulseVM.swift
@@ -0,0 +1,8 @@
+//
+//  PulseVM.swift
+//  The Brief
+//
+//  Created by Sami Gündoğan on 20.09.2025.
+//
+
+import Foundation
