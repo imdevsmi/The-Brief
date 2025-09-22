a26bc49369a5b55c94bbafc38593a93b9427a6ab
Sami Gündoğan <hsnsmi51@gmail.com>
2025-09-22 09:40:45 +0300

    add: weatherapiservice and weathermodel .swift files

---
+++ The Brief/Models/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  The Brief
+//
+//  Created by Sami Gündoğan on 22.09.2025.
+//
+
+import Foundation
---
+++ The Brief/Service/WeatherAPIService.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherAPIService.swift
+//  The Brief
+//
+//  Created by Sami Gündoğan on 22.09.2025.
+//
+
+import Foundation
