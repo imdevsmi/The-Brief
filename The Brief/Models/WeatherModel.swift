36eef95d35415deb96daad568f6618746d6ff853
Sami Gündoğan <hsnsmi51@gmail.com>
2025-09-22 14:27:42 +0300

    add: Implement Codable structs for Weather API (location, current, forecast)

--- The Brief/Models/WeatherModel.swift
+++ The Brief/Models/WeatherModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct WeatherResponse: Codable {
+    let location: Location
+    let current: Current
+    let forecast: Forecast
+}
+
+struct Location: Codable {
+    let name: String
+    let region: String
+    let country: String
+}
+
+struct Current: Codable {
+    let temp_c: Double
+    let condition: Condition
+}
+
+struct Condition: Codable {
+    let text: String
+    let icon: String
+}
+
+struct Forecast: Codable {
+    let forecastday: [ForecastDay]
+}
+
+struct ForecastDay: Codable {
+    let date: String
+    let day: Day
+}
+
+struct Day: Codable {
+    let maxtemp_c: Double
+    let mintemp_c: Double
+}
