75ab3070fa89b449eb24d1412e2c150a74b11a46
Sami Gündoğan <hsnsmi51@gmail.com>
2025-09-24 17:54:35 +0300

    feat: add WeatherCardView skeleton with configure method

--- The Brief/Utilities/WeatherCardView.swift
+++ The Brief/Utilities/WeatherCardView.swift
@@ -5,4 +5,18 @@
 //  Created by Sami Gündoğan on 24.09.2025.
 //
 
-import Foundation
+import SnapKit
+import UIKit
+
+final class WeatherCardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) { fatalError() }
+    
+    func configure(with model: WeatherUIModel) {
+        
+    }
+}
