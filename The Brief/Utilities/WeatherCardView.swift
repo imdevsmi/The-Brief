ebc2955d4943d48c623072fde57bd7d15e54899e
Sami Gündoğan <hsnsmi51@gmail.com>
2025-09-25 09:50:36 +0300

    add: weather card view iconImageView, cityLabel, tempLabel

--- The Brief/Utilities/WeatherCardView.swift
+++ The Brief/Utilities/WeatherCardView.swift
@@ -10,6 +10,27 @@ import UIKit
 
 final class WeatherCardView: UIView {
     
+    private let iconImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private let cityLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textColor = .label
+        return label
+    }()
+    
+    private let tempLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 32)
+        label.textColor = .label
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
