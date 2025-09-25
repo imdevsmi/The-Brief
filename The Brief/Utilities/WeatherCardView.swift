e1cad935f8c7f2768e2ecd236ea56ca9eecd78d6
Sami Gündoğan <hsnsmi51@gmail.com>
2025-09-25 10:04:35 +0300

    add: weather card view private func SetupUI

--- The Brief/Utilities/WeatherCardView.swift
+++ The Brief/Utilities/WeatherCardView.swift
@@ -54,9 +54,38 @@ final class WeatherCardView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        SetupUI()
     }
     
     required init?(coder: NSCoder) { fatalError() }
+    
+    private func SetupUI() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 12
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.15
+        layer.shadowRadius = 6
+        layer.shadowOffset = CGSize(width: 0, height: 3)
+        
+        let vStack = UIStackView(arrangedSubviews: [cityLabel, tempLabel, rangeLabel, aqiLabel, alertsLabel])
+        vStack.axis = .vertical
+        vStack.spacing = 6
+        vStack.alignment = .leading
+        
+        let hStack = UIStackView(arrangedSubviews: [iconImageView, vStack])
+        hStack.axis = .horizontal
+        hStack.alignment = .center
+        hStack.spacing = 12
+        
+        addSubview(hStack)
+        hStack.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(16)
+        }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(60)
+        }
+    }
     
     func configure(with model: WeatherUIModel) {
         
