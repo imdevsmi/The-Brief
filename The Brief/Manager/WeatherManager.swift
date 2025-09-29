263dd8615475a83068b93c47b649aedaa6382624
Sami Gündoğan <hsnsmi51@gmail.com>
2025-09-29 13:36:22 +0300

    userDefaults weather storage integration

--- The Brief/Manager/WeatherManager.swift
+++ The Brief/Manager/WeatherManager.swift
@@ -14,15 +14,22 @@ protocol WeatherManagerProtocol {
 }
 
 final class WeatherManager: WeatherManagerProtocol {
+    private let key = "lastCity"
+    private let defaults: UserDefaults
+    
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+    
     func saveCity(_ city: String) {
-        
+        defaults.set(city, forKey: key)
     }
     
     func loadCity() -> String? {
-        
+        defaults.string(forKey: key)
     }
     
     func clearCity() {
-        
+        defaults.removeObject(forKey: key)
     }
 }
