fba043f0b3001a070e30a79c19763849f48ef041
Sami Gündoğan <hsnsmi51@gmail.com>
2025-10-02 14:58:13 +0300

    feat(finance): implement fetchRates with rates/find endpoint

--- The Brief/Service/FinanceAPIService.swift
+++ The Brief/Service/FinanceAPIService.swift
@@ -20,6 +20,15 @@ final class FinanceAPIService: FinanceAPIServiceProtocol {
     init(networkManager: NetworkManagerProtocol = NetworkManager()) { self.networkManager = networkManager }
     
     func fetchRates(pairs: [String], completion: @escaping (Result<FinanceRatesResponse, NetworkError>) -> Void) {
+        var urlComponents = URLComponents(string: baseURL + "rates/find")
+        urlComponents?.queryItems = [URLQueryItem(name: "currency_pair", value: pairs.joined(separator: ","))]
         
+        guard let url = urlComponents?.url else {
+            completion(.failure(.invalidRequest))
+            return
+        }
+        
+        let headers = ["X-Auth-Token": SecureConfig.financeApiKey, "Content-Type": "application/json"]
+        networkManager.request(url: url, method: .GET, headers: headers, completion: completion)
     }
 }
