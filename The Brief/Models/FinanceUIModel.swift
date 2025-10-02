520105f21853e94bef38be301405201fa82be71e
Sami Gündoğan <hsnsmi51@gmail.com>
2025-10-02 15:07:45 +0300

    add: finance ui model

--- The Brief/Models/FinanceUIModel.swift
+++ The Brief/Models/FinanceUIModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct FinanceUIModel {
+    let pair: String
+    let bid: String
+    let offer: String
+}
+
+extension FinanceUIModel {
+    static func fromResponse(_ response: FinanceRatesResponse) -> [FinanceUIModel] {
+        response.rates.map { pair, rate in
+            FinanceUIModel(pair: pair, bid: rate.bid, offer: rate.offer)
+        }
+    }
+}
