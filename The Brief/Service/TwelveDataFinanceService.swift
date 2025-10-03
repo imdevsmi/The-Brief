706a1b2fee9bc28a3790e7a743eb27d94d6c9354
Sami Gündoğan <hsnsmi51@gmail.com>
2025-10-03 17:42:43 +0300

    feat(finance): add fetchRates definition to FinanceAPIServiceProtocol

--- The Brief/Service/TwelveDataFinanceService.swift
+++ The Brief/Service/TwelveDataFinanceService.swift
@@ -7,10 +7,15 @@
 
 import Foundation
 
-final class TwelveDataFinanceService {
+final class TwelveDataFinanceService: FinanceAPIServiceProtocol{
+    
     private let networkManager: NetworkManagerProtocol
     private let baseURL = "https://api.twelvedata.com"
     private let apiKey = SecureConfig.twelveDataApiKey
     
     init(networkManager: NetworkManagerProtocol = NetworkManager()) { self.networkManager = networkManager }
+    
+    func fetchRates(pairs: [String], completion: @escaping (Result<[FinanceUIModel], NetworkError>) -> Void) {
+        
+    }
 }
