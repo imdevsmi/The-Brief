c4529e2d4e5e140d1213293029350f253c5b5664
Sami Gündoğan <hsnsmi51@gmail.com>
2025-10-14 11:55:16 +0300

    feat: InterstitialAd class

---
+++ The Brief/Manager/AdMob/InterstitialAd.swift
@@ -0,0 +1,13 @@
+//
+//  InterstitialAd.swift
+//  The Brief
+//
+//  Created by Sami Gündoğan on 14.10.2025.
+//
+
+import GoogleMobileAds
+import UIKit
+
+final class InterstitialManager: NSObject {
+    
+}
