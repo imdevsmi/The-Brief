b99d5b0aa2ceb43f2664d5cfa3f42bad61fb45cc
Sami Gündoğan <hsnsmi51@gmail.com>
2025-10-16 13:47:25 +0300

    refactoring: remove comment line and add mark in InterstitialAd

--- The Brief/Manager/AdMob/InterstitialAd.swift
+++ The Brief/Manager/AdMob/InterstitialAd.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class AdManager: NSObject {
     
-    // MARK: -Properties
+    // MARK: - Properties
     static let shared = AdManager()
     private var interstitial: InterstitialAd?
     private var interstitialCounter = 0
@@ -35,19 +35,15 @@ final class AdManager: NSObject {
     // MARK: - Display Logic
     func showInterstitialIfNeeded(from viewController: UIViewController) {
         interstitialCounter += 1
-        // Load the interstitial if it doesn't exist
         if interstitial == nil { loadInterstitial() }
         
         switch interstitialCounter {
         case 1, 2:
-            // first two clicks don't show the ad
             break
         default:
-            // from the 3. click onwards
             if let interstitial = interstitial {
                 interstitial.present(from: viewController)
             } else {
-                // if interstitial isn't ready on 3rd click reload it
                 loadInterstitial()
             }
         }
